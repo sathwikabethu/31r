b3ec90f73227d4b9028db61f6230178eebdf1476
sathwikabethu <bethusathwika@gmail.com>
2025-04-08 19:01:01 +0530

    portfolio

---
+++ python/day5.py
@@ -0,0 +1,15 @@
+list_1=[1,2,3,45]
+tuple_1=(23,45,56)
+string_1="hello"
+int_1=78
+float_num=45.23
+complex_value=2+7j
+bool_value=False
+print(list_1,type(list_1))
+print(tuple_1,type(tuple_1))
+print(string_1,type(string_1))
+print(int_1,type(int_1))
+print(float_num,type(float_num))
+print(complex_value,type(complex_value))
+print(bool_value,type(bool_value))
+
