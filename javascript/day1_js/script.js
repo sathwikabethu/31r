9b00cc41049900037d7691e5d87bcad3c51998ad
sathwikabethu <bethusathwika@gmail.com>
2025-04-10 23:06:51 +0530

    js started

---
+++ javascript/day1_js/script.js
@@ -0,0 +1,45 @@
+function showAlertExternal(){
+    alert('external javascript');
+}
+// nummber 
+let num=10;
+let num2=12.5;
+// string
+var str="my string and this can't be changed"
+// boolean
+let boolean=true;
+// undefined
+var a;
+// null
+let numOfItems=null;
+
+// nonprimitive datatypes
+// array
+let arr=["this","is","array",10,90.11,true];
+// object
+let object={
+    key:"value_pairs",
+    name:"sathwika",
+    surname:"bethu"
+};
+// function
+function functionName(){
+    console.log("uff! completing a boring assignment");
+}
+
+// consoling these variables
+console.log(num,typeof num);
+console.log(num2,typeof num2);
+console.log(str,typeof str);
+console.log(boolean,typeof boolean);
+console.log(numOfItems,typeof numOfItems);
+console.log(arr,typeof arr);
+console.log(object,typeof object);
+console.log(functionName(),typeof functionName());
+
+
+
+
+
+
+
