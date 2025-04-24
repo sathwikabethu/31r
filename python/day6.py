30586aceb48d89ec8fdf383b9e0ad7229557473c
sathwikabethu <bethusathwika@gmail.com>
2025-04-24 18:48:24 +0530

    python files

--- python/day6.py
+++ python/day6.py
@@ -1,5 +1,12 @@
-num=int(input("enter a number:"))
-if num%2==0:
-    print("even")
-else:
-    print("odd")
+# num=int(input("enter a number:"))
+# if num%2==0:
+#     print("even")
+# else:
+#     print("odd")
+def multipleOf3(num):
+    if num%3==0:
+        return True
+    return False
+
+print(multipleOf3(44))
+
